715033526970b4b180237d2bb3ba6494f751bfd4
guilhermeassuncao <cgmatosassuncao@gmail.com>
2021-06-22 21:05:49 -0300

    Maximum Speed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import { useHistory } from "react-router";
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
-import '../styles/auth.scss'
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
-export function Home () {
+import "../styles/auth.scss";
+
+export function Home() {
+    const history = useHistory();
+    const { user, signInWithGoogle } = useAuth();
+
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+        history.push("/rooms/new");
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -13,21 +27,19 @@ export function Home () {
                 <p>Tire duas dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo Google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>
-                        ou entre em uma sala
-                    </div>
+                    <div className="separator">ou entre em uma sala</div>
                     <form>
                         <input type="text" placeholder="Digite o código da sala" />
-                        <button type="submit">Entrar na sala</button>
+                        <Button type="submit">Entrar na sala</Button>
                     </form>
                 </div>
             </main>
         </div>
-    )
+    );
 }
